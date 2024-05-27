ed717a74bc00301aee152abd636de4db93f26da5
FangoJett <wera.michal@gmail.com>
2024-05-27 23:56:38 +0200

    updated main page

--- app/RootLayout.js
+++ app/RootLayout.js
@@ -1,5 +1,7 @@
 "use client";
 
+
+
 import { useState } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -12,6 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   const [selectedPage, setSelectedPage] = useState(1);
+
 
   let pageContent;
   switch (selectedPage) {
@@ -32,37 +35,47 @@ export default function RootLayout({ children }) {
   }
 
   return (
-    <div className="layout">
-      <div className="sidebar">
-        <h1 className="app-title">APP</h1>
-        <div className="card" onClick={() => setSelectedPage(1)}>
-          <h2>
-            Konsola 
-          </h2>
-          <p>Wyslij polecenie do plawy</p>
-        </div>
-        <div className="card" onClick={() => setSelectedPage(2)}>
-          <h2>
-            Mapa 
-          </h2>
-          <p>Oznacz obszar działania pławy</p>
-        </div>
-        <div className="card" onClick={() => setSelectedPage(3)}>
-          <h2>
-            Dane LORA 
-          </h2>
-          <p>Pokaż dane z LORAwan</p>
-        </div>
-        <div className="card" onClick={() => setSelectedPage(4)}>
-          <h2>
-            Dane pomiarowe 
-          </h2>
-          <p>
-            Wyswietl dane pomiarowe
-          </p>
-        </div>
+
+  
+    
+      
+<div className="layout">
+    <div className="sidebar">
+
+      <div className="logo-container">
+      <img  src="/images/logo.png" alt="App Logo" className="app-logo" />
+      <div  className="app-title">
+            A<span className="orange-letter">P</span>P
+          </div>
       </div>
-      <div className="content">{pageContent}</div>
+
+
+
+      <div className="card" onClick={() => setSelectedPage(1)}>
+        <h2>Konsola</h2>
+        <p>Wyślij polecenie do pływającego urządzenia</p>
+      </div>
+      <div className="card" onClick={() => setSelectedPage(2)}>
+        <h2>Mapa</h2>
+        <p>Oznacz obszar działania pływającego urządzenia</p>
+      </div>
+      <div className="card" onClick={() => setSelectedPage(3)}>
+        <h2>Dane LORA</h2>
+        <p>Pokaż dane z LORAwan</p>
+      </div>
+      <div className="card" onClick={() => setSelectedPage(4)}>
+        <h2>Dane pomiarowe</h2>
+        <p>Wyświetl dane pomiarowe</p>
+      </div>
     </div>
+    
+    <div className="rightSide"> 
+    <div className="top-bar">
+    <p className="top-text">Nagłówek na górze strony</p>
+  </div>
+    <div className="content">{pageContent}</div>
+  </div>
+</div>
+   
   );
 }
